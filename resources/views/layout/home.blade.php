0404793a66d4c6fdbf4024eba4e39a95235658bd
johnferijrr <johnferijr@gmail.com>
2023-04-26 07:33:37 +0700

    fe

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -1,19 +1,61 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            Welcome
-        </div>
-        <div class="card-body">
-            <div class="alert alert-info">
-                Selamat datang di project pertama laravel 10
-            </div>
-        </div>
-    </div>
-    <button type="button" class="btn btn-primary" onclick="myhello()">Hello</button>
+    <div class="ay"></div>
+    <img class="au" src="images/image 15.png">
+    <div class="r"></div>
+    <div class="s"></div>
+    <div class="t"></div>
+    <div class="u"></div>
+    <div class="v"></div>
     <p class="a">Platform</p>
     <p class="b">Digitalisasi</p>
     <p class="c">E-Sarpras Vokasi</p>
+    <p class="d">E-Sarpras merupakan inovasi layanan dari Sekolah Vokasi UGM dalam membangun sistem sarana dan prasarana yang dapat diakses oleh seluruh civitas akademis UGM</p>
+    <p class="e">15+</p>
+    <p class="f">200+</p>
+    <p class="g">50+</p>
+    <p class="h">Ruang & Lab</p>
+    <p class="i">Tools & Hardware</p>
+    <p class="w">Device</p>
+    <p class="j">SARANA DAN PRASARANA</p>
+    <p class="k">PANDUAN PEMINJAMAN</p>
+    <p class="l">PERTANYAAN YANG SERING DITANYAKAN</p>
+    <p class="m">LOKASI</p>
+    <p class="n">Target Output E-Sarpras</p>
+    <p class="o">Sebagai platform digital Sarana dan Prasarana dengan fitur monitoring realtime</p>
+    <p class="p">Fasilitas peminjaman berbasis web untuk meningkatkan layanan Civitas Akademik</p>
+    <p class="q">Memaksimalkan potensi fasilitas yang ada di DTEDI untuk meningkatkan potensi akademik</p>
+    <p class="aj">1</p>
+    <p class="x">2</p>
+    <p class="y">3</p>
+    <p class="z">4</p>
+    <p class="aa">STEP</p>
+    <p class="ab">STEP</p>
+    <p class="ac">STEP</p>
+    <p class="ad">STEP</p>
+    <p class="ae">Ini Sample</p>
+    <p class="af">Ini Sample</p>
+    <p class="ag">Ini Sample</p>
+    <p class="ah">Ini Sample</p>
+    <img class="ai" src="images/Group.png">
+    <img class="aj" src="images/Frame.png">
+    <img class="ak" src="images/Frame (1).png">
+    <img class="al" src="images/Frame (2).png">
+    <div class="am"></div>
+    <div class="an"></div>
+    <div class="ao"></div>
+    <div class="ap"></div>
+    <div class="aq"></div>
+    <div class="ar"></div>
+    <div class="as"></div>
+    <div class="at"></div>
+    <p class="au">Departemen Teknik Elektro dan Informatika Sekolah Vokasi Universitas Gadjah Mada Jl. Yacaranda, Sekip Unit III, Yogyakarta. 55281 tedi.sv@ugm.ac.id (0274) 6491302, 561111 | (0274) 542908</p>
+    <p class="av">E-Sarpras DTEDI SV UGM</p>
+    <p class="aw">© DEPARTEMEN TEKNIK ELEKTRO DAN INFORMATIKA SEKOLAH VOKASI UGM</p>
+    <img class="ax" src="images/Lambang UGM-putih (1).png">
+    <img class="az" src="images/Logo Horizontal 2.png">
+    <a href="#" class="ba">Masuk</a>
+    <a href="#" class="bb">Daftar Akun</a>
 
 @endsection
