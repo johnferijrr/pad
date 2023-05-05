dab72165688fb1a608292252665155502d1d7a85
johnferijrr <johnferijr@gmail.com>
2023-05-05 09:58:29 +0700

    new

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -166,28 +166,33 @@
     <img class="bw" src="images/l-1.png">
     <img class="bx" src="images/l-2.png">
     <img class="by" src="images/l-3.png">
-        <!-- Menyisipkan library Google Maps -->
+    <div class="bz">
         <script src="http://maps.googleapis.com/maps/api/js"></script>
-    
         <script>
-            // fungsi initialize untuk mempersiapkan peta
-            function initialize() {
-            var propertiPeta = {
-                center:new google.maps.LatLng(-8.5830695,116.3202515),
-                zoom:9,
-                mapTypeId:google.maps.MapTypeId.ROADMAP
-            };
-            
-            var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
-            }
-    
-            // event jendela di-load  
-            google.maps.event.addDomListener(window, 'load', initialize);
+        function initialize() {
+          var propertiPeta = {
+            center:new google.maps.LatLng(-8.5830695,116.3202515),
+            zoom:9,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+          };
+          
+          var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+          
+          // membuat Marker
+          var marker=new google.maps.Marker({
+              position: new google.maps.LatLng(-7.774588123293439, 110.37450370080577),
+              map: peta
+          });
+        
+        }
+        
+        // event jendela di-load  
+        google.maps.event.addDomListener(window, 'load', initialize);
         </script>
 
-        <!-- Elemen yang akan menjadi kontainer peta -->
-        <div id="googleMap" style="width:100%;height:380px;"></div>
-
+          <div id="googleMap" style="width:100%;height:380px;"></div>
+          
+</div>
 
 
 @endsection
